e44f5d0ffe9852e08c9f2b57503909d09376e865
Sanmay Chavan <sanmaychavan22@gmail.com>
2025-09-03 10:39:59 +0530

    Fix API baseURL to switch between local and deployed backend

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,22 @@
+// import axios from "axios";
+
+
+// const api = axios.create({
+//     baseURL: "http://localhost:5001/api"
+// })
+
+// export default api
+
+
 import axios from "axios";
 
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "https://notes-app-backend-plum.vercel.app/api"
+    : "http://localhost:5001/api";
 
 const api = axios.create({
-    baseURL: "http://localhost:5001/api"
-})
+  baseURL: baseURL,
+});
 
-export default api
+export default api;
